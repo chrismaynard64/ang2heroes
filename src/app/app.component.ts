d8ed228d87d40055433d4dc015fdc9e88e036e60
chrismaynard64 <chrismaynard64@gmail.com>
2016-09-18 16:58:04 +0100

    Revert "Initial"
    This reverts commit 291db9c3a724c58acf492ae0e25d40e95590797f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Hero} from './shared/hero';
-import { HEROES } from './shared/mock-heroes'
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  
-
-  constructor()  {
-    this.heroes = HEROES;
-  }
-
-
+  title = 'app works!';
 }
