291db9c3a724c58acf492ae0e25d40e95590797f
chrismaynard64 <chrismaynard64@gmail.com>
2016-09-18 16:55:35 +0100

    Initial
    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Hero} from './shared/hero';
+import { HEROES } from './shared/mock-heroes'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  
+
+  constructor()  {
+    this.heroes = HEROES;
+  }
+
+
 }
